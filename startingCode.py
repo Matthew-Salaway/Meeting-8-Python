6a8789cbcc79f79fd7ea40f696a20858cee43367
Matthew Salaway <matthewsalaway@gmail.com>
2020-11-24 12:38:17 -0700

    Create startingCode.py

---
+++ startingCode.py
@@ -0,0 +1,63 @@
+# Copy this code into your own repl. Click the navigation bar in the top left corner and go to My Repl.
+# Click the New Repl on the left, create the repl, and paste the code. Or put into another IDE.
+
+# There will be a school wide coding competition on December 9, 2020!
+# It will be held via zoom, and will be in random groups of 3 or 4
+# The first team to complete 3 challenges will be the winner (30 minutes total)
+# The winning team members will each receive $10 gift cards
+
+# We will be doing a short review over python to prepare for the competition
+# Today we will do a quick review and next meeting we will finish reviewing
+# The week of the coding competition, we will skip the club meeting and go to the competition
+# The week after that will be finals week so no meetings after that, until after break.
+# For the second semester we will be diving into data science!
+
+# But now for the review
+
+# Print "Hello World" to the terminal
+
+
+
+# Create a variable of each type - Types: String, Int, Double, Boolean
+
+
+
+
+
+
+# Print the variable
+
+
+
+# Here are 2 vaiables; Print out "[name] is [age] years old!"
+name = "Jimmy"
+age = 12         # *** Dont forget to convert the int into a string ***
+
+
+# Jimmy is now 8 years older. Update the age variable by adding 8, making the computer calculate
+
+
+
+# Grace is 1.5 times older than Jimmy. Create the "grace" variable by mulipling jimmy's age by 1.5
+
+
+
+# We want to know Jimmy's first initial. Create the firstInitial varaible, and assign it Jimmy's first letter
+# by referencing the name variable and using [index] to select the first letter.
+
+
+
+# We want to know the users age. Create the variable "userAge"
+# And assign it to the value the user inputs with the input() function
+
+
+
+# Grace and Jimmy are coworkers and want to get a drink after work. The waiter needs to make sure they
+# are over 21. Grace looks old enough, but the waiter is unsure about Jimmy
+
+# Create an if/else statement using the age variable to see if the age is 21 or over
+# If the age is 21 or over, print "You look young for your age"
+# If the age is under 21, print "Nice try Jimmy"
+
+
+
